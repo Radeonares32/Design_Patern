bc5207988ada1e8bc4460dfca7509c8e29bf5285
Radeonares32 <radeonares@gmail.com>
2021-08-10 13:29:48 +0300

    NullObject and Multiton design patern

---
+++ NullObject/NullObject/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(StopLogger.GetLogger());
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        private ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            _logger.Log();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Log4NetLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
+    }
+
+    class NLogLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with NLogger");
+        }
+    }
+
+    class StopLogger:ILogger
+    {
+        private static StopLogger _stopLogger;
+        private static object _lock = new object();
+        private StopLogger() { }
+
+        public static StopLogger GetLogger()
+        {
+            lock (_lock)
+            {
+                if (_stopLogger == null)
+                {
+                    _stopLogger = new StopLogger();
+                }
+            }
+
+            return _stopLogger;
+        }
+        public void Log()
+        {
+            
+        }
+    }
+
+    class CustomerManagerTests
+    {
+        public void SaveTest()
+        {
+            CustomerManager customerManager = new CustomerManager(StopLogger.GetLogger());
+            customerManager.Save();
+        }
+    }
+    
+}
